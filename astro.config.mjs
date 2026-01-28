e3956ae9494e956eb24abdf673df10dd8ac0dc1a
Abahumar <abahumarziyad@gmail.com>
2026-01-28 12:50:46 +0800

    chore: Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  site: 'https://abahumar.github.io',
+  base: '/kfoodie',
   integrations: [mdx()]
 });
